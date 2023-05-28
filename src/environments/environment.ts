19a046d6132e6bf2c9ebec7c7d4ed02cda74ad0b
robertIsaac <dev.robert.isaac@gmail.com>
2023-05-28 03:10:39 +0300

    add ng fire

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'fire-esbuild-reprod',
+    appId: '1:954564512627:web:b2e388ceaedf0996dc1306',
+    storageBucket: 'fire-esbuild-reprod.appspot.com',
+    apiKey: 'AIzaSyCD10AIdfFw7daFLd8vo2BF4kp757dA7OU',
+    authDomain: 'fire-esbuild-reprod.firebaseapp.com',
+    messagingSenderId: '954564512627',
+  },
+};
